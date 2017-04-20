4922853bfd31b7639696b753fffd8c17270baeda
Bruno Tafur <brunotafur@pe.ibm.com>
2017-04-20 11:08:34 -0500

    prueba inicial

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
+			
 				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
